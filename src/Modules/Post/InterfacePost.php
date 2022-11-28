b2fb529fac2e293295c20a5be679f5eb2b7b4e63
Ilya <59627636+Freshblood1121@users.noreply.github.com>
2022-11-28 03:30:09 +0300

    Update InterfacePost.php
    Signed-off-by: Ilya <59627636+Freshblood1121@users.noreply.github.com>

--- src/Modules/Post/InterfacePost.php
+++ src/Modules/Post/InterfacePost.php
@@ -6,16 +6,4 @@ use App\Contracts\AddInterface;
 
 interface InterfacePost implements AddInterface
 {
-    public function __toString(): string
-    {
-        return $this->user . 'Пишет пост: ' . $this->text;
-    }
-
-    /**
-     * @return int
-     */
-    public function getId(): int
-    {
-        return $this->id;
-    }
 }
