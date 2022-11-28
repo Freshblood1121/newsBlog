215b138eab761ddfbd2349f701053587486d4a08
Ilya <59627636+Freshblood1121@users.noreply.github.com>
2022-11-28 03:27:45 +0300

    Update InterfaceComment.php
    Signed-off-by: Ilya <59627636+Freshblood1121@users.noreply.github.com>

--- src/Modules/Comment/InterfaceComment.php
+++ src/Modules/Comment/InterfaceComment.php
@@ -2,16 +2,8 @@
 
 namespace App\Modules\Comment;
 
-use App\Contracts\a\AddInterface;
+use App\Contracts\AddInterface;
 
 interface InterfaceComment implements AddInterface
 {
-    public function __toString(): string
-    {
-        return $this->user . 'Пишет комментарий: ' . $this->text;
-    }
-    public function getId(): int
-    {
-        return $this->id;
-    }
 }
